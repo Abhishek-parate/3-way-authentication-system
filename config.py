3d19674c16d8f596e828f1b4f965541e184730a8
Abhishek-parate <abhishek.parate.mcm@ghrccst.raisoni.net>
2025-03-22 12:44:10 +0530

    remove unused files and project structure for Flask authentication

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "super_secret_key")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///auth.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    OTP_EXPIRY_SECONDS = 300  # 5 minutes
